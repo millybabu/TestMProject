a5bf2f5d5fc5246b08752a6821ccb436242d90d1
manoj <manoj@Dell>
2021-07-17 16:10:04 +0530

    third change

--- TestSelenium/src/main/java/com/sample/pages/GitDemo.java
+++ TestSelenium/src/main/java/com/sample/pages/GitDemo.java
@@ -3,7 +3,7 @@ package com.sample.pages;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		System.out.println("First addition");
+		
 		System.out.println("Second addition");
 	}
 
