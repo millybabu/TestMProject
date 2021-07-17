604bac512f56873b5dd9014e2e0a32339814abbd
manoj <manoj@Dell>
2021-07-17 16:02:20 +0530

    changing GitDemo

--- TestSelenium/src/main/java/com/sample/pages/GitDemo.java
+++ TestSelenium/src/main/java/com/sample/pages/GitDemo.java
@@ -3,8 +3,8 @@ package com.sample.pages;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("First addition");
+		System.out.println("Second addition");
 	}
 
 }
