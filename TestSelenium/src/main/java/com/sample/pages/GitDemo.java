391f9b9e085c1c9ed29e49c4ea679da094362a32
manoj <manoj@Dell>
2021-07-18 11:17:01 +0530

    commiting my changes-method1

--- TestSelenium/src/main/java/com/sample/pages/GitDemo.java
+++ TestSelenium/src/main/java/com/sample/pages/GitDemo.java
@@ -1,6 +1,10 @@
 package com.sample.pages;
 
 public class GitDemo {
+	
+	public void method1(){
+		System.out.println("this is my change");
+	}
 
 	public static void main(String[] args) {
 	System.out.println("First addition");
